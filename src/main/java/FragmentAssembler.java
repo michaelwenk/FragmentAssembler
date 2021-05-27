d51a5a45844a1b8f43a22fe24f5ea39a0a0e107d
michaelwenk <michael.wenk@uni-jena.de>
2021-05-27 11:27:41 +0200

    feat: added possibility for multiple start SSCs

--- src/main/java/FragmentAssembler.java
+++ src/main/java/FragmentAssembler.java
@@ -32,28 +32,62 @@ public class FragmentAssembler {
         System.out.println(querySpectrum);
         System.out.println(mf);
 
+        final int nStarts = 1;
         final int maxSphere = 3;
         final int minMatchingSphereCount = 1;
-        final double shiftTol = 2;
-        final double matchFactorThrs = 2;
+        final double shiftTol = 5;
+        final double matchFactorThrs = 4;
+        final int nThreads = 3;
         //        List<SSC> sscList = Fragmentation.buildFromNMRShiftDB("/Users/mwenk/Downloads/test.sd", new String[]{"13C"},
-        //                                                              maxSphere, 2);
+        //                                                                    maxSphere, nThreads);
+        //        List<SSC> sscList = Fragmentation.buildFromNMRShiftDB("/Users/mwenk/Downloads/nmrshiftdb2withsignals.sd",
+        //                                                              new String[]{"13C"}, maxSphere, nThreads);
         List<SSC> sscList = new ArrayList<>();
 
         final String pathToJsonFile = "/Users/mwenk/Downloads/sscList.json";
+        //        final String pathToJsonFile = "/Users/mwenk/Downloads/sscList_all_3.json";
         //        Converter.sscListToJSONFile(sscList, pathToJsonFile);
 
         try {
-            sscList = Converter.jsonFileToSscList(pathToJsonFile, 2);
-        } catch (final InterruptedException e) {
-            e.printStackTrace();
-        } catch (final FileNotFoundException e) {
+            sscList = Converter.jsonFileToSscList(pathToJsonFile, nThreads);
+            System.out.println("\ntotal SSC count: "
+                                       + sscList.size());
+        } catch (final InterruptedException | FileNotFoundException e) {
             sscList = new ArrayList<>();
             e.printStackTrace();
         }
 
+        //        final Map<String, Double[]> hoseCodeShiftStatistics = Utils.buildHOSECodeShiftStatistics(sscList);
+        //        System.out.println("statistics:\n");
+        //        for (final Map.Entry<String, Double[]> stringEntry : hoseCodeShiftStatistics.entrySet()) {
+        //            System.out.println(stringEntry.getKey()
+        //                                       + ": "
+        //                                       + Arrays.toString(stringEntry.getValue()));
+        //        }
+
         sscList = sscList.parallelStream()
                          .filter(ssc -> {
+                             if (!AssemblyUtils.compareWithMolecularFormula(ssc.getStructure(), mf)) {
+                                 return false;
+                             }
+
+                             if (ssc.getSpectrum()
+                                    .getSignalCountWithEquivalences()
+                                     > querySpectrum.getSignalCountWithEquivalences()) {
+                                 return false;
+                             }
+
+                             final Double averageDeviation = Match.calculateAverageDeviation(ssc.getSpectrum(),
+                                                                                             querySpectrum, 0, 0,
+                                                                                             shiftTol, true, true,
+                                                                                             true);
+                             if (averageDeviation
+                                     != null
+                                     && averageDeviation
+                                     > matchFactorThrs) {
+                                 return false;
+                             }
+
                              final Assignment matchAssignment = Match.matchSpectra(ssc.getSpectrum(), querySpectrum, 0,
                                                                                    0, shiftTol, true, true, true);
                              return matchAssignment.getSetAssignmentsCountWithEquivalences(0)
@@ -62,10 +96,9 @@ public class FragmentAssembler {
                                      - Utils.countHeteroAtoms(ssc.getStructure());
                          })
                          .collect(Collectors.toList());
-
         Utils.sortSSCList(sscList, querySpectrum, shiftTol);
 
-        System.out.println("\ntotal SSC count: "
+        System.out.println("\nfiltered SSC count: "
                                    + sscList.size());
 
         final DepictionGenerator depictionGenerator = new DepictionGenerator().withAtomColors()
@@ -75,7 +108,10 @@ public class FragmentAssembler {
                                                                               .withFillToFit();
         //        try {
         //            for (int i = 0; i
-        //                    < sscList.size(); i++) {
+        //                    < sscList.size()
+        //                    && i
+        //                    < 100; i++) {
+        //                //                            < sscList.size(); i++) {
         //                depictionGenerator.depict(sscList.get(i)
         //                                                 .getStructure())
         //                                  .writeTo("/Users/mwenk/Downloads/depictions/ssc"
@@ -86,70 +122,60 @@ public class FragmentAssembler {
         //            e.printStackTrace();
         //        }
 
+        // prepared for possible start SSC selection
+        final List<Integer> startSSCIndices = new ArrayList<>();
+        for (int i = 0; i
+                < nStarts; i++) {
+            startSSCIndices.add(i);
+        }
+
         final SmilesGenerator smilesGenerator = new SmilesGenerator(SmiFlavor.Absolute);
         final Map<String, SSC> solutions = new HashMap<>();
-        final Stack<Object[]> intermediates = new Stack<>();
-
-        final int startSSCIndex = 0;
-        intermediates.push(new Object[]{sscList.get(startSSCIndex).buildClone(), startSSCIndex});
-
+        Stack<Object[]> intermediates;
         Object[] stackItem;
         SSC intermediate;
-        int currentSSCIndex;
-        SSC currentSSC;
+        int currentSSCIndex, startSSCIndex;
         List<SSC> extendedSSCList;
         String smiles;
-        Map<Integer, List<Integer[]>> overlaps;
+        boolean stop;
+        for (final int i : startSSCIndices) {
+            intermediates = new Stack<>();
+            intermediates.push(new Object[]{sscList.get(i).buildClone(), i, 0});
 
-        while (!intermediates.empty()) {
-            stackItem = intermediates.pop();
-            intermediate = (SSC) stackItem[0];
-            currentSSCIndex = ((int) stackItem[1])
-                    + 1;
-            if (currentSSCIndex
-                    >= sscList.size()) {
-                continue;
-            }
+            while (!intermediates.empty()) {
+                stackItem = intermediates.pop();
+                intermediate = (SSC) stackItem[0];
+                startSSCIndex = (int) stackItem[1];
+                currentSSCIndex = ((int) stackItem[2]);
 
-            //            if (currentSSCIndex
-            //                    == startSSCIndex) {
-            //                continue;
-            //            }
-            System.out.println("\n\n--> ssc pair: "
-                                       + startSSCIndex
-                                       + " vs. "
-                                       + currentSSCIndex);
-            currentSSC = sscList.get(currentSSCIndex);
-            overlaps = AssemblyUtils.getOverlaps(intermediate, currentSSC, minMatchingSphereCount);
-            extendedSSCList = new ArrayList<>();
-            for (final Map.Entry<Integer, List<Integer[]>> integerListEntry : overlaps.entrySet()) {
-                System.out.println("---> sphere match: "
-                                           + integerListEntry.getKey());
-                for (final Integer[] rootAtomIndices : integerListEntry.getValue()) {
-                    System.out.println("\n--> root atom pair: "
-                                               + Arrays.toString(rootAtomIndices));
-                    for (final SSC extendedSSC : Assembly.assemblyCore(querySpectrum, mf, maxSphere,
-                                                                       intermediate.buildClone(), currentSSC,
-                                                                       rootAtomIndices[0], rootAtomIndices[1], shiftTol,
-                                                                       matchFactorThrs)) {
-                        extendedSSCList.add(extendedSSC);
-                        try {
-                            smiles = smilesGenerator.create(extendedSSC.getStructure());
+                if (currentSSCIndex
+                        >= sscList.size()) {
+                    continue;
+                }
+                stop = false;
+                extendedSSCList = new ArrayList<>();
+                for (int j = currentSSCIndex; j
+                        < sscList.size(); j++) {
+                    if (j
+                            == startSSCIndex) {
+                        continue;
+                    }
+                    //                    System.out.println("\n\n--> ssc pair: "
+                    //                                               + startSSCIndex
+                    //                                               + " vs. "
+                    //                                               + j);
+                    extendedSSCList.addAll(
+                            Assembly.assemblyOverlaps(intermediate, sscList.get(j), querySpectrum, mf, null,//maxSphere,
+                                                      shiftTol, matchFactorThrs, minMatchingSphereCount));
+                }
+                if (!extendedSSCList.isEmpty()) {
+                    Utils.sortSSCList(extendedSSCList, querySpectrum, shiftTol);
+                    Utils.removeDuplicatesFromSSCList(sscList, new HashSet<>());
+                    Collections.reverse(extendedSSCList);
+                    for (final SSC extendedSSC : extendedSSCList) {
+                        if (AssemblyUtils.isFinalSSC(extendedSSC, querySpectrum, shiftTol, matchFactorThrs, mf)) {
                             try {
-                                depictionGenerator.depict(extendedSSC.getStructure())
-                                                  .writeTo("/Users/mwenk/Downloads/depictions/extended_"
-                                                                   + startSSCIndex
-                                                                   + "-"
-                                                                   + currentSSCIndex
-                                                                   + "_"
-                                                                   + rootAtomIndices[0]
-                                                                   + "-"
-                                                                   + rootAtomIndices[1]
-                                                                   + ".png");
-                            } catch (final IOException | CDKException e) {
-                                e.printStackTrace();
-                            }
-                            if (AssemblyUtils.isFinalSSC(extendedSSC, querySpectrum, shiftTol, matchFactorThrs, mf)) {
+                                smiles = smilesGenerator.create(extendedSSC.getStructure());
                                 solutions.putIfAbsent(smiles, extendedSSC);
                                 try {
                                     depictionGenerator.depict(extendedSSC.getStructure())
@@ -158,28 +184,43 @@ public class FragmentAssembler {
                                                                        + "-"
                                                                        + currentSSCIndex
                                                                        + "_"
-                                                                       + rootAtomIndices[0]
-                                                                       + "-"
-                                                                       + rootAtomIndices[1]
+                                                                       + smiles
                                                                        + ".png");
                                 } catch (final IOException | CDKException e) {
                                     e.printStackTrace();
                                 }
+                                stop = true;
+                            } catch (final CDKException e) {
+                                e.printStackTrace();
                             }
-                        } catch (final CDKException e) {
-                            e.printStackTrace();
+                        } else {
+                            //                            try {
+                            //                                smiles = smilesGenerator.create(extendedSSC.getStructure());
+                            //                                depictionGenerator.depict(extendedSSC.getStructure())
+                            //                                                  .writeTo("/Users/mwenk/Downloads/depictions/extended_"
+                            //                                                                   + startSSCIndex
+                            //                                                                   + "-"
+                            //                                                                   + currentSSCIndex
+                            //                                                                   + "_"
+                            //                                                                   + smiles
+                            //                                                                   + "_"
+                            //                                                                   + extendedSSC.getUnsaturatedAtomIndices()
+                            //                                                                   + ".png");
+                            //                            } catch (final IOException | CDKException e) {
+                            //                                e.printStackTrace();
+                            //                            }
+
+                            intermediates.push(new Object[]{extendedSSC.buildClone(), startSSCIndex, currentSSCIndex
+                                    + 1});
                         }
                     }
+                } else {
+                    intermediates.push(new Object[]{intermediate, startSSCIndex, currentSSCIndex
+                            + 1});
                 }
-            }
-            if (!extendedSSCList.isEmpty()) {
-                Utils.sortSSCList(extendedSSCList, querySpectrum, shiftTol);
-                Collections.reverse(extendedSSCList);
-                for (final SSC newIntermediate : extendedSSCList) {
-                    intermediates.push(new Object[]{newIntermediate.buildClone(), currentSSCIndex});
+                if (stop) {
+                    break;
                 }
-            } else {
-                intermediates.push(new Object[]{intermediate, currentSSCIndex});
             }
         }
 
